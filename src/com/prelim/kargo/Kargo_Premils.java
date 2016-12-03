13ffb154d7d91fc49716e963cb9e016d23f9ff51
Frantz <Frantz-b@github.com>
2016-12-03 06:07:45 -0500

    Properly added implicit wait to find website element.  AND was able to click on setting menu

--- src/com/prelim/kargo/Kargo_Premils.java
+++ src/com/prelim/kargo/Kargo_Premils.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import java.util.concurrent.TimeUnit;
 
 public class Kargo_Premils {
 
@@ -14,10 +15,16 @@ public class Kargo_Premils {
 		
 		WebDriver cdriver = new ChromeDriver();
 		cdriver.get("http://www.google.com");
+		cdriver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 		
 		WebElement mike = cdriver.findElement(By.id("lst-ib"));
 		String arg0 = "kargo";
 		mike.sendKeys(arg0 + Keys.RETURN);
+		
+		
+		cdriver.findElement(By.linkText("Website")).click();
+		
+		cdriver.findElement(By.className("nav-button__bar")).click();
 		//Keys ky = new Keys();
 		//mike.sendKeys(Keys.RETURN);
 		
