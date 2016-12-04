25b43b8d8d2e4b45821652cd4c18cf768eaa3661
Frantz <Frantz-b@github.com>
2016-12-04 03:13:57 -0500

    Successfully created method not only to read lines from a file and put into an array, but also created a method that generates a text file from an given array.
    For the method that reads a file, I purposely made it an array list because I wanted it to be scalable.
    
    Also was reminded that if I do not make methods throwable, I need to put them in a try-catch block.

--- src/com/prelim/kargo/Kargo_Premils.java
+++ src/com/prelim/kargo/Kargo_Premils.java
@@ -5,25 +5,80 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
-import java.io.*;
 
-public class Kargo_Premils  {
+public class Kargo_Premils {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
+		/*
+		String chromeDriverLoc = "/Users/NewUser/Downloads/Automation/chromedriver";
+		System.setProperty("webdriver.chrome.driver", chromeDriverLoc);
 		
-		// writing string to a file encoded as modified UTF-8
-	      DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("file2016.txt"));
-	      dataOut.writeUTF("hello");
-	      dataOut.writeUTF("dez");
-	      dataOut.writeUTF("nuts");
-
-	      // Reading data from the same file
-	      DataInputStream dataIn = new DataInputStream(new FileInputStream("file2016.txt"));
-
-	      while(dataIn.available()>0) {
-	         String k = dataIn.readUTF();
-	         System.out.print(k+" ");
-	      }
-	   }
+		WebDriver cdriver = new ChromeDriver();
+		cdriver.get("http://www.google.com");
+		cdriver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		
+		WebElement mike = cdriver.findElement(By.id("lst-ib"));
+		String arg0 = "kargo";
+		mike.sendKeys(arg0 + Keys.RETURN);
+		
+		
+		cdriver.findElement(By.linkText("Website")).click();
+		
+		cdriver.findElement(By.className("nav-button__bar")).click();
+		cdriver.findElement(By.id("menu-item-6427")).click();
+		
+		cdriver.get("http://www.google.com");
+		WebElement me = cdriver.findElement(By.id("lst-ib"));
+		String arg1 = "kargo";
+		me.sendKeys(arg1 + Keys.RETURN);
+		
+		
+		// Mobile ad firm
+		// Mobile advertising
+		// Mobile marketing
+		// Mobile internet */
+		createKeywordsFile();
+		ArrayList<String> keyWord_Cap = keyWord_Capture();
+		System.out.println(keyWord_Cap.size());
 	}
+	
+	public static void createKeywordsFile() {
+		String[] keyWords = {"Mobile ad firm" , "Mobile advertising" , "Mobile marketing" , "Mobile internet"};
+		FileWriter key_Word_File = null;
+		
+		try {
+			key_Word_File = new FileWriter("Keywords.txt");
+			for(String key_Word : keyWords){
+				key_Word_File.write(key_Word + "\n");
+			}
+			key_Word_File.close();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static ArrayList<String> keyWord_Capture(){
+		ArrayList<String> keyWords_Captured = new ArrayList<String>();
+		String fileLocation = "Keywords.txt";
+		Scanner fileReader = null;
+		
+		try{
+			fileReader = new Scanner(new File(fileLocation));
+			while (fileReader.hasNext()){
+				keyWords_Captured.add(fileReader.nextLine());
+			}
+			fileReader.close();
+		} catch (IOException e){
+			e.printStackTrace();
+		}	
+		return keyWords_Captured;
+	}
+
+}
